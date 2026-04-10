07a35c7ca3f7a91d1309f6c761ade94e50753f13
timothy-miriti <timothy.miriti@student.moringaschool.com>
2026-04-10 22:27:18 +0300

    add javascript code

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
+require('datejs');
 
+function combineUsers(...args) {
+  const combinedObject = {
+    users: []
+  };
+
+  for (let i = 0; i < args.length; i++) {
+    const currentArray = args[i];
+    combinedObject.users = [...combinedObject.users, ...currentArray];
+  }
+
+  combinedObject.merge_date = new Date().toString("M/d/yyyy");
+
+  return combinedObject;
+}
+
+module.exports = combineUsers;
 
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
+
